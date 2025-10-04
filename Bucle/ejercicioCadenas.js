ffe81e5efd778b4d7c9259841208954039df5d13
HancelE1993 <hancelxyz@gmail.com>
2025-10-04 15:36:13 -0500

    se agrega funcion ejecutarPrueba2 e invertir casdena

---
+++ Bucle/ejercicioCadenas.js
@@ -0,0 +1,44 @@
+ejecutarPrueba1 = function () {
+    let mensaje;
+    mensaje = recuperarTexto("txtCadena");
+    recorrerCadena(mensaje);
+
+}
+
+recorrerCadena = function (cadena) {
+    //012
+    //Juan
+    let caracter;
+
+    for (let posicion = 0; posicion < cadena.length; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("Caracter " + caracter + " posicion " + posicion);
+    }
+    for (let posicion = 0; posicion <= cadena.length - 1; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("Caracter " + caracter + " posicion " + posicion);
+    }
+}
+
+
+ejecutarPrueba2 = function () {
+    let mensaje;
+    mensaje = recuperarTexto("txtCadena");
+    let resultado = invertirCadena(mensaje);
+    mostrarTexto("lblResultado", resultado);
+}
+
+
+invertirCadena = function (cadena) {
+    //012
+    //Juan
+    let caracter;
+    let resultado = "";
+
+    for (let posicion = cadena.length - 1; posicion >= 0; posicion--) {
+        caracter = cadena.charAt(posicion);
+        resultado += caracter;
+        console.log(" posicion " + posicion + "Caracter " + caracter);
+    }
+    return resultado;
+} 
