03763b85dbd190df302755ab0640d3f3217dc703
HancelE1993 <hancelxyz@gmail.com>
2025-10-06 15:12:15 -0500

    Paso 3

--- Juego ahorcado/ahorcado.js
+++ Juego ahorcado/ahorcado.js
@@ -1,17 +1,17 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 //Paso 0
-esMayuscula=function(caracter){
-    let codigo=caracter.charCodeAt(0);
-    if(codigo>=65 &&codigo<=90){
+esMayuscula = function (caracter) {
+    let codigo = caracter.charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90) {
         return true
-    }else{
+    } else {
         return false
     }
 }
 //Paso 1
-let palabraSecreta="";
-guardarPalabra=function(){
-    let palabra=document.getElementById("txtSecreta").value;
+let palabraSecreta = "";
+guardarPalabra = function () {
+    let palabra = document.getElementById("txtSecreta").value;
 
     if (palabra.length !== 5) {
         alert("Digite una palabra de 5 letras mayúsculas");
@@ -30,11 +30,21 @@ guardarPalabra=function(){
 }
 
 //Paso 2
-mostraLetra=function(letra,posicion){
-let idDiv = div + posicion;
-let div = document.getElementById(idDiv);
-if (div) {
+mostraLetra = function (letra, posicion) {
+    let idDiv = div + posicion;
+    let div = document.getElementById(idDiv);
+    if (div) {
         div.textContent = letra;
     }
 
+}
+//Paso 3
+let letrasEncontradas = 0;
+validar = function (letra) {
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta[i] === letra) {
+            mostrarLetra(letra, i);   
+            letrasEncontradas++;      
+        }
+    }
 }
