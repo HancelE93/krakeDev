41ef73d1448fba009542b76338da90aa087343dd
HancelE1993 <hancelxyz@gmail.com>
2025-10-06 15:46:33 -0500

    Paso 5

--- Juego ahorcado/ahorcado.js
+++ Juego ahorcado/ahorcado.js
@@ -1,19 +1,19 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//variables globales
+let palabraSecreta = "";
+let coincidencias = 0;
+let intentos = 0;
+let errores = 0;
 //Paso 0
 esMayuscula = function (caracter) {
     let codigo = caracter.charCodeAt(0);
-    if (codigo >= 65 && codigo <= 90) {
-        return true
-    } else {
-        return false
-    }
+    return (codigo >= 65 && codigo <= 90)
 }
 //Paso 1
-let palabraSecreta = "";
 guardarPalabra = function () {
     let palabra = document.getElementById("txtSecreta").value;
 
-    if (palabra.length !== 5) {
+    if (palabra.length != 5) {
         alert("Digite una palabra de 5 letras mayúsculas");
         return false;
     }
@@ -39,23 +39,47 @@ mostraLetra = function (letra, posicion) {
 
 }
 //Paso 3
-let letrasEncontradas = 0;
 validar = function (letra) {
+ let letraEncontrada = false;
+
     for (let i = 0; i < palabraSecreta.length; i++) {
-        if (palabraSecreta[i] === letra) {
-            mostrarLetra(letra, i);   
-            letrasEncontradas++;      
+        if (palabraSecreta[i] == letra) {
+            mostrarLetra(letra, i);
+            coincidencias++;
+            letraEncontrada = true;
         }
     }
-}
 
+    if (!letraEncontrada) {
+        alert("LA LETRA NO ES PARTE DE LA PALABRA");
+        errores++;
+    }
+}
 //Paso 4
 ingresarLetra=function(){
 
     let letra = document.getElementById("txtLetra").value;
 
+    
+    if (!letra) {
+        alert("Ingrese una letra");
+        return;
+    }
+
     if (esMayuscula(letra)) {
+        intentos++;
         validar(letra);
+
+        
+        if (coincidencias == 5) {
+            alert("!!HA GANADO!!");
+        }
+
+    
+        if (intentos == 10) {
+            alert("!!HA PERDIDO!!");
+        }
+
     } else {
         alert("SOLO INGRESAR MAYÚSCULAS");
     }
