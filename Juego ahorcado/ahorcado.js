3c39366791acd750c20e0a5c3676887a281daa82
HancelE1993 <hancelxyz@gmail.com>
2025-10-06 14:37:53 -0500

    Paso 1

--- Juego ahorcado/ahorcado.js
+++ Juego ahorcado/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//Paso 0
 esMayuscula=function(caracter){
     let codigo=caracter.charCodeAt(0);
     if(codigo>=65 &&codigo<=90){
@@ -6,4 +7,24 @@ esMayuscula=function(caracter){
     }else{
         return false
     }
+}
+//Paso 1
+let palabraSecreta="";
+guardarPalabra=function(){
+    let palabra=document.getElementById("txtSecreta").value;
+
+    if (palabra.length !== 5) {
+        alert("Digite una palabra de 5 letras mayúsculas");
+        return false;
+    }
+
+    for (let letra of palabra) {
+        if (letra < 'A' || letra > 'Z') {
+            alert("Digite una palabra de 5 letras mayúsculas");
+            return false;
+        }
+    }
+    palabraSecreta = palabra;
+    alert("VALIDACION COMPLETA")
+    return true;
 }
