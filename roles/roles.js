bc724f30160b1247484b043cc2cf1912cc60549d
HancelE1993 <hancelxyz@gmail.com>
2025-10-13 15:20:56 -0500

    reto 47 habilitar boton guardar y cajas de texto

--- roles/roles.js
+++ roles/roles.js
@@ -1,53 +1,67 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
-    {cedula:"1723439319", nombre:"Hancel",apellido:"Espin",sueldo:800}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1723439319", nombre: "Hancel", apellido: "Espin", sueldo: 800 }
 ]
 
-mostrarEmpleados=function(){
-    let cmpTabla=document.getElementById("tablaEmpleados");
-    let contenidoTabla="<table class=\"miTabla\"><tr>" +
-    "<th>CEDULA</th>"+
-    "<th>NOMBRE</th>"+
-    "<th>APELLIDO</th>"+
-    "<th>SULEDO</th>"+
-    "</tr>";
+let esNuevo=false;
+
+mostrarEmpleados = function () {
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table class=\"miTabla\"><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SULEDO</th>" +
+        "</tr>";
     let elementoEmpleados;
-    for(let i=0;i<empleados.length;i++){
-        elementoEmpleados=empleados[i];
-        contenidoTabla+=
-        "<tr><td>"+elementoEmpleados.cedula+ "</td>"
-        +"<td>"+elementoEmpleados.nombre+ "</td>"
-        +"<td>"+elementoEmpleados.apellido+ "</td>"
-        +"<td>"+elementoEmpleados.sueldo+ "</td>"
-        +"</tr>"
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleados = empleados[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoEmpleados.cedula + "</td>"
+            + "<td>" + elementoEmpleados.nombre + "</td>"
+            + "<td>" + elementoEmpleados.apellido + "</td>"
+            + "<td>" + elementoEmpleados.sueldo + "</td>"
+            + "</tr>"
     }
-    contenidoTabla+="</table>"
-    cmpTabla.innerHTML=contenidoTabla;
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
 }
 
 
 
-mostrarOpcionEmpleado=function(){
-
+mostrarOpcionEmpleado = function () {
+    let esNuevo=true;
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
     mostrarEmpleados()
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
 }
 
-mostrarOpcionRol=function(){
-    
+mostrarOpcionRol = function () {
+
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
     ocultarComponente("divResumen");
 }
 
-mostrarOpcionResumen=function(){
+mostrarOpcionResumen = function () {
 
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
-    
+
 }
 
+ejecutarNuevo = function () {
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+}
